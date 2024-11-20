36114ec7fcfd2e6feb9b44b5f6d10f1f1bcf8c2c
lmiceli <elpacho@gmail.com>
2024-11-21 00:47:52 +0100

    improved mouse

--- keyboards/bastardkb/charybdis/3x5/keymaps/lmiceli/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/lmiceli/rules.mk
@@ -3,3 +3,15 @@ MOUSEKEY_ENABLE = yes        # Mouse keys
 RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
 RGBLIGHT_ENABLE = no
 BACKLIGHT_ENABLE = no
+
+INDICATOR_LIGHTS = no      # Enable indicator lights for caps lock, etc.
+COMBO_ENABLE = no          # Custom key combos
+EXTRAKEY_ENABLE = yes
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+OLED_ENABLE = no
+
+DEBOUNCE_TYPE = asym_eager_defer_pk
+AUTO_SHIFT_ENABLE = no
+BACKLIGHT_ENABLE = no
+
