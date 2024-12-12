79dfc038cdb6a25a483fd15ecb3ea973a584d220
lmiceli <elpacho@gmail.com>
2024-12-12 18:10:18 +0100

    fix? usb disconnect

--- keyboards/bastardkb/charybdis/3x5/keymaps/lmiceli/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/lmiceli/keymap.c
@@ -9,6 +9,17 @@
 
 //#define SLH_MOU LT(_MOU, KC_SLSH)
 
+enum layers {
+    _COLEMAK = 0,
+    _NAVIGATION,
+    _ARROW,
+    _MOUSE,
+    _SYMBOL,
+    _NUMBER,
+    _FUNCTION,
+    _LAYER_MOUSE,/*auto for trackball*/
+};
+
 enum userspace_keycodes {
     /* SCLN_DRG = CHARYBDIS_SAFE_RANGE, */
     SCLN_DRG = SAFE_RANGE,
@@ -66,18 +77,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true; // We didn't handle other keypresses
 }
-
-/* new for sync most of my existing keymaps */
-enum layers {
-    _COLEMAK = 0,
-    _NAVIGATION,
-    _ARROW,
-    _MOUSE,
-    _SYMBOL,
-    _NUMBER,
-    _FUNCTION,
-    _LAYER_MOUSE,/*auto for trackball*/
-};
 
 /* // clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
