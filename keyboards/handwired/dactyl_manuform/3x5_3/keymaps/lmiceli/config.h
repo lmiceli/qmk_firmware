7a848b79520a50a698c7bacf9014cb2754d989a6
lmiceli <elpacho@gmail.com>
2025-05-25 20:20:55 +0200

    got folder again from old branch

---
+++ keyboards/handwired/dactyl_manuform/3x5_3/keymaps/lmiceli/config.h
@@ -0,0 +1,51 @@
+
+#pragma once
+
+//#define UNUSED_PINS
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+/* Serial settings */
+//#define USE_SERIAL
+/* serial.c configuration for split keyboard */
+//#define SOFT_SERIAL_PIN D2
+
+// #define USE_SERIAL_PD2
+
+/* Select hand configuration */
+//#define MASTER_LEFT
+//#define MASTER_RIGHT
+#define EE_HANDS
+
+// Set the mouse settings to a comfortable speed/accuracy trade-off,
+// assuming a screen refresh rate of 60 Htz or higher
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+#define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+#define MOUSEKEY_TIME_TO_MAX 80
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+#define MOUSEKEY_DELAY 100
+// It makes sense to use the same delay for the mouseweel
+#define MOUSEKEY_WHEEL_DELAY 100
+// The default is 100
+#define MOUSEKEY_WHEEL_INTERVAL 50
+// The default is 40
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 190
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+
+// for ipad
+//#ifndef USB_MAX_POWER_CONSUMPTION
+//#    define USB_MAX_POWER_CONSUMPTION 40
+//#endif
