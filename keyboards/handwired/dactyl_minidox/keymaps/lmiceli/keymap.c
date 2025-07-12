961e7f16a489689e82aa08ee3e822e3dfbd08649
lmiceli <elpacho@gmail.com>
2025-07-12 12:45:36 +0200

    more scroll is always better

--- keyboards/handwired/dactyl_minidox/keymaps/lmiceli/keymap.c
+++ keyboards/handwired/dactyl_minidox/keymaps/lmiceli/keymap.c
@@ -143,13 +143,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MOUSE] = LAYOUT_split_3x5_3(
 
-        KC_NO,   KC_WH_U, ALT_TAB, KC_WH_D, KC_NO, /**/ KC_NO, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS,
+        QK_BOOT, KC_WH_U, ALT_TAB, KC_WH_D, KC_NO, /**/ KC_NO, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS,
 
         KC_NO,   KC_LALT, KC_LGUI, KC_LSFT, KC_NO, /**/ KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,
 
-        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO, /**/ KC_NO, KC_WH_L, KC_BTN1,   KC_WH_R, KC_NO,
+        KC_WH_U, KC_WH_U, KC_NO,   KC_WH_D, KC_NO, /**/ KC_NO, KC_WH_L, KC_BTN1,   KC_WH_R, KC_NO,
 
-                        KC_TRNS, KC_BTN1,  KC_TRNS,    KC_BTN3, KC_BTN1, KC_BTN2
+                       KC_BTN2, KC_BTN1,  KC_BTN2,      KC_BTN3, KC_BTN1, KC_BTN2
 
         ),
 
